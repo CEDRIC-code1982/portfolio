c790cf53222b0f65cce7174d744c4a0adf4fccd8
CEDRIC-code1982 <cedric.pineau007@gmail.com>
2021-11-17 15:09:01 +0100

    in progress

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-//import './styles.scss';
+import './styles.css';
 
 const Projects = () => (
     <div className='home'>
       <div className='react'>
-      <h1>Projet React</h1>
+      <h1>Projets React</h1>
+      <ul className='react-project'>
+        <li><a href= "https://convertisseur.surge.sh/">Convertisseur</a></li>
+        <li><a href= "https://cp-todo.surge.sh/">Todolist</a></li>
+        <li><a href= "https://gradient-vanilla.surge.sh/">Gradient</a></li>
+        <li><a href= "https://apigit.surge.sh/">Api Git</a></li>
+      </ul>
       </div>
-      
+      <div className='native'>
+      <h1>Projets React Native</h1>  
+      </div>
     </div>
   
   );
